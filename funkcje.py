bafda829c23d9e354ee4629d67c552d4f12f1a7a
Filip-Fedor <118117604+Filip-Fedor@users.noreply.github.com>
2023-02-17 17:37:16 +0100

    Add files via upload

--- funkcje.py
+++ funkcje.py
@@ -32,3 +32,31 @@ def wspolne_lata(emisja, gdp, populacja):
     lata_wspolne.sort()
     return lata_wspolne
 
+
+def wspolne_kraje(emisja, gdp, populacja):
+    emisja_kraje = emisja['Country'].tolist()
+    emisja_kraje1 = list(dict.fromkeys(emisja_kraje))
+    gdp_kraje = gdp['Country Name'].tolist()
+    gdp_kraje1 = list(dict.fromkeys(gdp_kraje))
+    populacja_kraje = populacja['Country Name'].tolist()
+    populacja_kraje1 = list(dict.fromkeys(populacja_kraje))
+    return przeciecie(emisja_kraje1, gdp_kraje1, populacja_kraje1)
+
+
+def lista_tabel_kraj_rok(tabela, lata_wspolne):
+    lista_tabel = []
+    for i in range(len(lata_wspolne)):
+        lista_tabel.append(tabela[['Country Name', lata_wspolne[i]]])
+    return lista_tabel
+
+
+def zmiana_nazwy(lista_tabel, nazwa):
+    for i in range(len(lista_tabel)):
+        lista_tabel[i].columns = ['Country', nazwa]
+
+
+def lista_tabel_polacz(l1, l2, l3):
+    lista_polaczonych_tabel = []
+    for i in range(len(l1)):
+        lista_polaczonych_tabel.append(pd.merge(pd.merge(l1[i], l2[i], on='Country'), l3[i], on='Country'))
+    return lista_polaczonych_tabel
