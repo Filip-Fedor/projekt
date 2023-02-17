1705a7816b5eac38c3a17660754a9b7d7362d984
Filip-Fedor <118117604+Filip-Fedor@users.noreply.github.com>
2023-02-17 14:23:53 +0100

    Add files via upload

--- funkcje.py
+++ funkcje.py
@@ -8,3 +8,27 @@ def czytaj_plik(sciezka, nazwa_pliku):
         return pd.read_csv(polacz_sciezke)
     else:
         return pd.read_csv(polacz_sciezke, skiprows=4)
+
+
+def przeciecie(a, b, c):
+    return list(set(a) & set(b) & set(c))
+
+
+def roznica_ab(a, b):
+    c = []
+    for el in a:
+        if el not in b:
+            c.append(el)
+    return c
+
+
+def wspolne_lata(emisja, gdp, populacja):
+    emisja_lata = emisja['Year'].tolist()
+    emisja_lata = list(dict.fromkeys(emisja_lata))
+    emisja_lata = [str(i) for i in emisja_lata]
+    populacja_lata = populacja.columns[4:].tolist()
+    gdp_lata = gdp.columns[4:].tolist()
+    lata_wspolne = przeciecie(emisja_lata, populacja_lata, gdp_lata)
+    lata_wspolne.sort()
+    return lata_wspolne
+
--- main.py
+++ main.py
@@ -11,5 +11,23 @@ args = parser.parse_args()
 
 
 emisja = czytaj_plik(args.sciezka_emisja, "fossil-fuel-co2-emissions-by-nation.csv")
-GDP = czytaj_plik(args.sciezka_GDP, "API_NY.GDP.MKTP.CD_DS2_en_csv_v2_4751562.csv")
+gdp = czytaj_plik(args.sciezka_GDP, "API_NY.GDP.MKTP.CD_DS2_en_csv_v2_4751562.csv")
 populacja = czytaj_plik(args.sciezka_populacja, "API_SP.POP.TOTL_DS2_en_csv_v2_4751604.csv")
+
+
+del populacja[populacja.columns[-1]]
+del gdp[gdp.columns[-1]]
+
+lata_wspolne = wspolne_lata(emisja, gdp, populacja)
+populacja_lata = populacja.columns[4:].tolist()
+gdp_lata = gdp.columns[4:].tolist()
+kol_usun_pop = roznica_ab(populacja_lata, lata_wspolne)
+kol_usun_gdp = roznica_ab(gdp_lata, lata_wspolne)
+lata_wspolne = [int(i) for i in lata_wspolne]
+emisja = emisja[emisja['Year'].isin(lata_wspolne)]
+gdp = gdp.drop(kol_usun_gdp, axis=1)
+populacja = populacja.drop(kol_usun_pop, axis=1)
+
+print(emisja)
+print(populacja)
+print(gdp)
