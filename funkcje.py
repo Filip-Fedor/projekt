bd0922b1ed88ff80b51797ede06bee850a555b32
Filip-Fedor <118117604+Filip-Fedor@users.noreply.github.com>
2023-02-19 17:22:12 +0100

    Add files via upload

--- funkcje.py
+++ funkcje.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 
 
+# Dostaje sciezke do pliku oraz nazwe pliku csv i zwraca dataframe
 def czytaj_plik(sciezka, nazwa_pliku):
     polacz_sciezke = os.path.join(sciezka, nazwa_pliku)
     if nazwa_pliku == "fossil-fuel-co2-emissions-by-nation.csv":
@@ -10,14 +11,18 @@ def czytaj_plik(sciezka, nazwa_pliku):
         return pd.read_csv(polacz_sciezke, skiprows=4)
 
 
+# Zwraca przeciecie 2 list
 def przeciecie2(a, b):
     return list(set(a) & set(b))
 
 
+# zwraca przeciecie 3 list
 def przeciecie3(a, b, c):
     return list(set(a) & set(b) & set(c))
 
 
+# Mamy 2 listy a i b.
+# Zwraca liste elementow ktore sa w a ale nie sa b.
 def roznica_ab(a, b):
     c = []
     for el in a:
@@ -26,6 +31,8 @@ def roznica_ab(a, b):
     return c
 
 
+# Mamy 2 liczby typu str (start, koniec)
+# Zwraca liste z intow w przedziale [int(start), int(koniec)]
 def przedzial_lat(start, koniec):
     lata = []
     for i in range(int(start), int(koniec)+1):
@@ -33,6 +40,11 @@ def przedzial_lat(start, koniec):
     return lata
 
 
+# Mamy 3 dataframy: emisja, gdp, populacja
+# Z df emisji wybieramy wszystkie wiersze z kolumny Year, czyli wszystkie lata, ktore sie znajduja w dataframie.
+# Dataframy: populacja, gdp maja kolumny: Country Name, Country Code, Indicator Name, Indicator Code, a pozniej kolumny z latami.
+# Wybieramy wszystkie kolumny z latami i wrzucamy do listy.
+# Na koncu przecinamy wszystkie 3 listy, aby dostac lata, ktore sa we wszystkich dataframach i sortujemy.
 def wspolne_lata(emisja, gdp, populacja):
     emisja_lata = emisja['Year'].tolist()
     emisja_lata = list(dict.fromkeys(emisja_lata))
@@ -44,6 +56,8 @@ def wspolne_lata(emisja, gdp, populacja):
     return lata_wspolne
 
 
+# Mamy 3 dataframy: emisja, gdp, populacja
+# Z kazdego dataframu wybieramy kolumne z nazwami krajow i wrzucamy do list, nastepnie zwracamy przeciecie tych 3 list.
 def wspolne_kraje(emisja, gdp, populacja):
     emisja_kraje = emisja['Country'].tolist()
     emisja_kraje1 = list(dict.fromkeys(emisja_kraje))
@@ -54,6 +68,8 @@ def wspolne_kraje(emisja, gdp, populacja):
     return przeciecie3(emisja_kraje1, gdp_kraje1, populacja_kraje1)
 
 
+# Mamy dataframe tabela, ktora zawiera kolumny: Country Name oraz kolumny z latami, ktore sa tez w liscie lata_wspolne
+# Zwraca liste dataframow z nazwa kolumny: Country Name, w ktorej sa nazwy panstw, oraz z rokiem.
 def lista_tabel_kraj_rok(tabela, lata_wspolne):
     lista_tabel = []
     for i in range(len(lata_wspolne)):
@@ -61,11 +77,14 @@ def lista_tabel_kraj_rok(tabela, lata_wspolne):
     return lista_tabel
 
 
+# Zamiana nazwy kolumny w dataframach z listy lista_tabel.
 def zmiana_nazwy(lista_tabel, nazwa):
     for i in range(len(lista_tabel)):
         lista_tabel[i].columns = ['Country', nazwa]
 
 
+# Dla 3 list jednakowej dlugosci, ktore zawieraja dataframy, ktore maja taka sama nazwe kolumny Country,
+# laczymy je po tej kolumnie.
 def lista_tabel_polacz(l1, l2, l3):
     lista_polaczonych_tabel = []
     for i in range(len(l1)):
@@ -73,6 +92,10 @@ def lista_tabel_polacz(l1, l2, l3):
     return lista_polaczonych_tabel
 
 
+# Dla danej listy dataframow (wszystkie z takimi samymi kolumnami, ale inne dane),
+# tworzymy liste dataframow, ktore zawieraja tylko czesc kolumn z poczatkowych dataframow.
+# nastepnie kazdego dataframa sortujemy po jendej z kolumn
+# zwraca dataframe, ktory zostal polaczony ze wszystkich df z nowej listy.
 def tabela_najwiecej_co2(tabele_emisja):
     tabele_rok = []
     for i in range(len(tabele_emisja)):
@@ -83,6 +106,11 @@ def tabela_najwiecej_co2(tabele_emisja):
     return tabela
 
 
+# Mamy liste dataframow, ktore maja miedzy innymi kolumny z rokiem, krajem, populacja, GDP,
+# Tworzymy nowa liste dataframow z danej listy z kolumanmi: 'Year', 'Country', 'Population', 'GDP'
+# Nastepnie dla kazdego dataframe w nowej liscie dodajemy nowa kolumne z wartosciami: GDP/Population
+# Sortujemy po nowo dodanej kolumnie i zostawiamy w kazdym dataframie tylko 5 gornych wierszy.
+# Laczymy wszystkie dataframy z listy w jeden dataframe i go zwracamy.
 def tabela_najwiekszy_przychod(tabele_emisja_pop_gdp):
     tabele_rok = []
     tabele_rok2 = []
@@ -96,6 +124,12 @@ def tabela_najwiekszy_przychod(tabele_emisja_pop_gdp):
     return tabela
 
 
+# Mamy dataframe emisja, oraz liste lat (niepusta) posortowana rosnaco
+# Tworzymy nowa liste z ostatnimi 10 latami, albo jesli lista jest krotsza to zostawiamy ja
+# Nastepnie tworzymy nowy dataframe z danego dataframa z kolumnami: 'Year', 'Country', 'Per Capita'
+# Nastepnie tworzymy dwa nowe dataframy, jeden z pierwszym rokiem z listy lat, drugi z ostatnim rokiem z listy lat
+# Pozniej laczymy te dwa dataframy po kolumnie 'Country' i tworzymy nowa kolumne z roznica:
+# Per Capita z  ostatniego roku z Per Capita z pierwszego roku
 def roznica_max_min(emisja, lata_wspolne):
     if len(lata_wspolne) > 10:
         lata_ostatnie = []
@@ -106,12 +140,6 @@ def roznica_max_min(emisja, lata_wspolne):
     emisja2 = emisja[['Year', 'Country', 'Per Capita']]
     emisja_rok_pierwszy = emisja2[emisja2['Year'] == lata_ostatnie[0]]
     emisja_rok_ostatni = emisja2[emisja2['Year'] == lata_ostatnie[-1]]
-    kraj_rok_pierwszy = emisja_rok_pierwszy['Country'].tolist()
-    kraj_rok_pierwszy = list(dict.fromkeys(kraj_rok_pierwszy))
-    kraj_rok_ostatni = emisja_rok_ostatni['Country'].tolist()
-    kraj_rok_ostatni = list(dict.fromkeys(kraj_rok_ostatni))
-    difference = list(set(kraj_rok_ostatni).symmetric_difference(set(kraj_rok_pierwszy)))
-    print(f"Kraje, ktorych nie bylo w danych: {difference}")
     wynik = pd.merge(emisja_rok_pierwszy, emisja_rok_ostatni, on="Country")
     wynik.loc[:, 'diff'] = wynik['Per Capita_y'] - wynik['Per Capita_x']
     wynik_max = wynik.sort_values('diff').head()
--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import argparse
-import pandas as pd
-import os
 from funkcje import *
 
 parser = argparse.ArgumentParser()
@@ -11,55 +9,71 @@ parser.add_argument("start_rok", help="Poczatek badanych lat")
 parser.add_argument("koniec_rok", help="Koniec badanych lat")
 args = parser.parse_args()
 
-
+# wczytanie dataframow
 emisja = czytaj_plik(args.sciezka_emisja, "fossil-fuel-co2-emissions-by-nation.csv")
 gdp = czytaj_plik(args.sciezka_GDP, "API_NY.GDP.MKTP.CD_DS2_en_csv_v2_4751562.csv")
 populacja = czytaj_plik(args.sciezka_populacja, "API_SP.POP.TOTL_DS2_en_csv_v2_4751604.csv")
 
-
+# usuniecie ostatnich kolumn, w ktorych nic nie bylo
 del populacja[populacja.columns[-1]]
 del gdp[gdp.columns[-1]]
 
+# tworzenie dwoch list: lista lat, ktore podal uzytkownik oraz lista lat wspolnych w 3 dataframach.
 lata_podane = przedzial_lat(args.start_rok, args.koniec_rok)
 lata_wspolne_tabele = wspolne_lata(emisja, gdp, populacja)
-
 lata_wspolne_str = przeciecie2(lata_podane, lata_wspolne_tabele)
 lata_wspolne_str.sort()
+
+# sprawdzamy czy lista jest pusta, jesli tak to informujemy i konczymy.
 if len(lata_wspolne_str) == 0:
     print("przedzial lat pusty")
     exit()
+
+# W dataframe populacja, gdp mamy kolumny liczac od 4 z danym rokiem, tworzymy liste wszystkich lat.
 populacja_lata = populacja.columns[4:].tolist()
 gdp_lata = gdp.columns[4:].tolist()
+# tworzymy listy lat, ktore sa w kolumnach dataframow (populacja, gdp), ale nie sa we wszystkich dataframach.
 kol_usun_pop = roznica_ab(populacja_lata, lata_wspolne_str)
 kol_usun_gdp = roznica_ab(gdp_lata, lata_wspolne_str)
 lata_wspolne_int = [int(i) for i in lata_wspolne_str]
+# Zostawiamy tylko te lata, ktore sa we wszystkich dataframach (emisja, gdp, populacja)
 emisja = emisja[emisja['Year'].isin(lata_wspolne_int)]
 gdp = gdp.drop(kol_usun_gdp, axis=1)
 populacja = populacja.drop(kol_usun_pop, axis=1)
 
 populacja['Country Name'] = populacja['Country Name'].str.upper()
 gdp['Country Name'] = gdp['Country Name'].str.upper()
+
+# lista panstw wystepujacych we wszystkich dataframe
 kraje_wspolne = wspolne_kraje(emisja, gdp, populacja)
+# Zostawiamy te kraje, ktore wystepuja we wszystkich dataframach
 emisja = emisja[emisja['Country'].isin(kraje_wspolne)]
 populacja = populacja[populacja['Country Name'].isin(kraje_wspolne)]
 gdp = gdp[gdp['Country Name'].isin(kraje_wspolne)]
 
+# Dla wspolnych lat tworzymy dla kazdej dataframe liste dataframow dla kazdego roku oddzielnie
 tabele_populacja = lista_tabel_kraj_rok(populacja, lata_wspolne_str)
 tabele_gdp = lista_tabel_kraj_rok(gdp, lata_wspolne_str)
 tabele_emisja = []
 for i in range(len(lata_wspolne_int)):
     tabele_emisja.append(emisja[emisja['Year'] == lata_wspolne_int[i]])
+
+
 zmiana_nazwy(tabele_populacja, 'Population')
 zmiana_nazwy(tabele_gdp, 'GDP')
 
+# Lista dataframow dla kazdego roku oddzielnie, z infomacja o emisji, populacji i gdp
 tabele_emisja_pop_gdp = lista_tabel_polacz(tabele_emisja, tabele_populacja, tabele_gdp)
 
+# Laczenie wszystkich dataframow w liscie tabele_emisja_pop_gdp w jeden dataframe.
 df_emisja_pop_gdp = pd.concat(tabele_emisja_pop_gdp)
 print(df_emisja_pop_gdp)
 
+# Dataframe z krajami w poszczegolnych latach, ktore produkuja najwiecej CO2 na mieszkanca.
 df_najwiecej_co2 = tabela_najwiecej_co2(tabele_emisja)
 print(df_najwiecej_co2)
 
+# Dataframe z krajami w poszeczegolnych latach, ktore maja najwiekszy przychod na mieszkanca.
 df_najwiekszy_przychod = tabela_najwiekszy_przychod(tabele_emisja_pop_gdp)
 print(df_najwiekszy_przychod)
 
